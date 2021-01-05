f55967cc17bec34509c7a990cce44c69df5af790
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-05 16:34:33 -0300

    Converters para List<String> a String y viceversa

--- app/src/main/java/cl/jav/pokemonesmvvm/Pokemon.kt
+++ app/src/main/java/cl/jav/pokemonesmvvm/Pokemon.kt
@@ -6,18 +6,24 @@ import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 
 @Entity(tableName = "pokemones")
-data class Pokemon(@PrimaryKey val id:String, val img:String, val name:String, val types:List<String>)
+data class Pokemon(
+    @PrimaryKey val id: String,
+    val img: String,
+    val name: String,
+    val types: List<String>
+)
 
 
+class Converter {
+    @TypeConverter
+    fun convertStringtoList(lista: String): List<String> {
 
-@TypeConverter
-fun convertStringtoList(lista:String):List<String>{
+        return lista.split(",")
+    }
 
-    return lista.split(",")
+    @TypeConverter
+    fun convertListtoString(lista: List<String>): String {
+
+        return lista.joinToString { "," }
+    }
 }
-@TypeConverter
-fun convertListtoString(lista:List<String>):String{
-
-    return lista.joinToString { "," }
-}
-
