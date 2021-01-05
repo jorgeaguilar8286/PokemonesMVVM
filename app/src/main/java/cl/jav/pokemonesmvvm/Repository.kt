1b77fd352d6c8f784454d7573b986ef21b7719a0
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-05 17:00:27 -0300

    aplicado el formato al repository

--- app/src/main/java/cl/jav/pokemonesmvvm/Repository.kt
+++ app/src/main/java/cl/jav/pokemonesmvvm/Repository.kt
@@ -1,28 +1,29 @@
 package cl.jav.pokemonesmvvm
 
-
+import android.util.Log
 import cl.jav.pokemonesmvvm.data.RoomApplication
 
 class Repository {
 
-    val pokedex= Pokedex()
-    val pokeDatabase=RoomApplication.pokeDatabase!!
+    val pokedex = Pokedex()
+    val pokeDatabase = RoomApplication.pokeDatabase!!
 
-    init{
+    init {
         loadPokedex()
     }
 
-   fun loadPokedex(){
-       if (isDatabaseempty()) {
-           pokeDatabase.daoPokemon().insertPokemon(pokedex.getLista())
-       }
-   }
+    fun loadPokedex() {
+        if (isDatabaseempty()) {
+            Log.d("Repository", "Creando información de pokemones")
+            pokeDatabase.daoPokemon().insertPokemon(pokedex.getLista())
+        }
+    }
 
-    fun isDatabaseempty():Boolean{
-        var result=false
+    fun isDatabaseempty(): Boolean {
+        var result = false
 
-        if(pokeDatabase.daoPokemon().countPokemon()==0){
-            result=true
+        if (pokeDatabase.daoPokemon().countPokemon() == 0) {
+            result = true
         }
 
         return result
@@ -30,14 +31,11 @@ class Repository {
 
 
     fun cargarPokemones(): List<Pokemon> {
-       return pokeDatabase.daoPokemon().getPokemones()
-
+        return pokeDatabase.daoPokemon().getPokemones()
     }
 
-    fun getPokemon(id: String):Pokemon {
+    fun getPokemon(id: String): Pokemon {
 
         return pokeDatabase.daoPokemon().getPokemon(id)
     }
-
-
 }
